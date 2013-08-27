9b0c4b00e896090d13906dd5767468dfe9ba226d
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2013-08-27 00:10:44 -0400

    can select answer from activeadmin

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register Question do
     f.inputs "Questions" do
       f.input :photo, :as => :file
       f.input :photo_mini, :as => :file
+      f.input :answer, :as => :select, :collection => Answer.all.map{|u| [u.answer, u.id]} 
     end
     f.buttons
   end
