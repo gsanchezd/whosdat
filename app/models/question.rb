698ac70b5605dd3b0c6ba24e1ee08709e9e983f9
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2013-08-27 01:05:22 -0400

    photo_mini now is an attraccesor

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  attr_accessible :answer_id, :photo
+  attr_accessible :answer_id, :photo, :photo_mini
   belongs_to :answer
   mount_uploader :photo, PhotoUploader
   mount_uploader :photo_mini, PhotoUploader
