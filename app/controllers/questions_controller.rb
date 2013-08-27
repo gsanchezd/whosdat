ed510491300d61154fc5130a46c0697dc1df84f6
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2013-08-27 11:27:11 -0400

    answer values are showed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,8 @@
 class QuestionsController < ApplicationController
 	def index
 		@question = Question.offset(rand(Question.count)).first
+		answers = Answer.offset(rand(Question.count)).first(3)
+		answers << @question.answer
+		@answers = answers.shuffle
 	end
 end
