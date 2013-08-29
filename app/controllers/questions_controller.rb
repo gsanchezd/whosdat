cb1e00aec0adbce8f9c66766839eb0c8ddc49cc1
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2013-08-29 18:46:39 -0400

    scores

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,17 @@ class QuestionsController < ApplicationController
 		answers = Answer.offset(rand(Question.count)).first(3)
 		answers << @question.answer
 		@answers = answers.shuffle
+		if session[:score].nil? 
+			session[:score] = 0
+		else
+			session[:score] = session[:score] + 1
+		end
+
 	end
 
 	def landing
 		@question = Question.offset(rand(Question.count)).first	
+		session[:score] = 0
 	end
 
 end
