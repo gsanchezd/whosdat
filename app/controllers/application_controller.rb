6a5d3fee46ef9ca25bfb40e42ac968fa81ff9db3
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2013-08-30 19:04:37 -0400

    removing comment no mobile version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+  def mobile_device?
+    request.env["HTTP_USER_AGENT"].try :match, /(iphone|ipod|android)/i
+  end
+  helper_method :mobile_device?
+
 end
