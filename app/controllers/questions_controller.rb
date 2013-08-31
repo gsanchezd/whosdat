950366eb2ac0d6d01cce1c649d0a62ac873bdde2
Gonzalo Sánchez <gonzalo.sanchez.d@gmail.com>
2013-08-31 01:12:21 -0400

    answer missing fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,8 @@
 class QuestionsController < ApplicationController
 	def index
 		@question = Question.offset(rand(Question.count)).first
-		answers = Answer.offset(rand(Question.count)).first(3)
+		#answers = Answer.offset(rand(Question.count)).first(3)
+		answers = Answer.order("RANDOM()").first(3)
 		answers << @question.answer
 		@answers = answers.shuffle
 		if session[:score].nil? 
